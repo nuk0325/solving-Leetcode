3c5426a6f1aabbe588ac02094a222e471f9a28f6
sunwook <117016927+nuk0325@users.noreply.github.com>
2025-01-21 17:45:44 -0600

    Time: 7 ms, Memory: 12.4 MB - LeetHub

--- height-checker/height-checker.py
+++ height-checker/height-checker.py
@@ -13,15 +13,12 @@ class Solution(object):
         for i in range(1, len(comList)) :
             if comList[i] < comList[i - 1] :
                 idx = i
-                print(idx)
                 while idx > 0 and comList[idx] < comList[idx - 1] :
                     comList[idx], comList[idx - 1] = comList[idx - 1], comList[idx]
                     idx -= 1
-            print(comList)
         
         count = 0
         for i in range(len(heights)) :
-            print(heights[i], comList[i])
             if heights[i] != comList[i] :
                 count += 1
                 
